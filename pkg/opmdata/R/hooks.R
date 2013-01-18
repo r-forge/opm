e2bc40a36f8adc8d5b78544fec8637fd0261f2d4
mgoeker (Markus Göker) <markus.goeker@dsmz.de>
2013-01-18 18:26:58 +0000

    added opmdata
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/opm@5 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/opmdata/R/hooks.R
@@ -0,0 +1,26 @@
+
+
+.onLoad <- function(...) {
+  opm.version <- utils::packageVersion("opm")
+  opmdata.version <- utils::packageVersion("opmdata")
+  opm.wanted <- package_version("0.7-28")
+  if (opm.version < opm.wanted) {
+    msg <- c(
+      "",
+      "PROBLEM when loading opmdata (version %s):",
+      "Object 'vaas_4' from this version is INCOMPATIBLE with opm < %s.",
+      "Please use a newer version of opm or an older version of opmdata.",
+      "Alternatively, convert old-style to new-style \"OPMS\" objects via:",
+      "",
+      "  vaas_4 <- as(as(vaas_4, \"list\"), \"OPMS\")",
+      "",
+      "(with opm >= %s!) before conducting any analyses with them.",
+      ""
+    )
+    msg <- paste(msg, collapse = "\n")
+    msg <- sprintf(msg, opmdata.version, opm.wanted, opm.wanted)
+    packageStartupMessage(msg)
+  }
+}
+
+
