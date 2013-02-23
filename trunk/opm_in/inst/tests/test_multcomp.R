28933fbe25cdd7e3429e713c29e679d67b273693
luebeck <luebeck@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2013-02-23 09:16:52 +0000

    tests fuer group_CI laufen, ACHTUNG: subset aus vaas_et_al wird im objects_for_testing() benoetigt. siehe email vom 23.02.2013 um 10:13 Uhr an MG
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/opm@33 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- trunk/opm_in/inst/tests/test_multcomp.R
+++ trunk/opm_in/inst/tests/test_multcomp.R
@@ -1,5 +1,9 @@
 
 library(testthat)
+library(pkgutils)
+
+pack_desc("C:\\Users\\lea\\Documents\\biolog 2013\\svn-next-try\\opm_in", "source")
+
 context("Testing the multiple-testing functions of the OPM package")
 
 
@@ -8,7 +12,7 @@ if (!exists("TEST.DIR"))
   attach(objects_for_testing())
 
 
-A.VALUES <- extract(c(THIN.AGG, THIN.AGG),
+A_VALUES <- extract(c(THIN.AGG, THIN.AGG),
   as.labels = list("organism", "run"), subset = "A", dataframe = TRUE)
 
 
