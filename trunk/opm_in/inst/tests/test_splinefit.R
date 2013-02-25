57f10b5a89b471475e80ceda9c7e56c8ebfbfd43
hofner (Benjamin Hofner) <benjamin.hofner@fau.de>
2013-02-25 16:49:10 +0000

    first additions for new spline fitting methods (not yet fully working) minor corrections in documentation
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/opm@40 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ trunk/opm_in/inst/tests/test_splinefit.R
@@ -0,0 +1,25 @@
+################################################################################
+#
+# CURRENTLY NO TESTS
+#
+
+## fit_spline
+## UNTESTED
+
+## AUC
+## UNTESTED
+
+## plot_helper
+## UNTESTED
+
+## get_data
+## UNTESTED
+
+## get_data.psp_model
+## UNTESTED
+
+## smooth
+## UNTESTED
+
+## Predict
+## UNTESTED
