2d2299be7d293b7015fd4fbe93642a74d4b5a51d
mgoeker <mgoeker@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2013-03-30 18:34:13 +0000

    extensions in OPM(S) indexing, bug fix in extract_columns(), new figure in vignette
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/opm@84 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- trunk/opm_in/R/constants.R
+++ trunk/opm_in/R/constants.R
@@ -169,6 +169,9 @@ W3C_COLORS <- structure(
 
 MEMOIZED <- new.env(parent = emptyenv())
 
+MEMOIZED$WELL.POS <- as.list(structure(.Data = seq.int(nrow(WELL_MAP)),
+  .Names = sort.int(rownames(WELL_MAP))))
+
 
 ################################################################################
 ################################################################################
