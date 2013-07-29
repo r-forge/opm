3d1d4d1744009e66a82bd0d9513ca3e8754ab8a8
mgoeker <mgoeker@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2013-07-29 18:29:41 +0000

    enhancements in opm_mcp()
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/opm@234 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- trunk/opm_in/inst/tests/test_multcomp.R
+++ trunk/opm_in/inst/tests/test_multcomp.R
@@ -295,18 +295,22 @@ test_that("linfct as predefined matrix-object", {
 
 
 ## opm_mcp
-test_that("non-syntactic names", {
-  y <- opm_mcp(EXPL.DF[, 1:10], output = "data", 
-  model = ~ J("Well", "run nonsys"), linfct = c(Pairs = 1))
+test_that("non-syntactic names can be present", {
+  x <- EXPL.DF[, 1:10]
+  colnames(x)[colnames(x) == "run"] <- "run nonsys"
+  y <- opm_mcp(x, output = "data",
+    model = ~ J(Well, `run nonsys`), linfct = c(Pairs = 1))
   expect_is(y, "data.frame")
   expect_equal(dim(y), c(28, 6))
 })
 
 
 ## opm_mcp
-test_that("linfct Pairs", {
-  y <- opm_mcp(EXPL.DF[, 1:10], output = "mcp", 
-  model = ~ J("Well", "run nonsys"), linfct = c(Pairs.Well = 1))
+test_that("'Pairs' contrast type can be combined with non-syntactic names", {
+  x <- EXPL.DF[, 1:10]
+  colnames(x)[colnames(x) == "run"] <- "run nonsys"
+  y <- opm_mcp(x, output = "mcp", model = ~ J(Well, `run nonsys`),
+    linfct = c(Pairs.Well = 1))
   expect_is(y, "glht")
   expect_equal(y$type, "User-defined")
   expect_true(is.list(y))
