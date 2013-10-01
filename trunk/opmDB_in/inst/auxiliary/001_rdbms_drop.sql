85d39fa14fcdb3784c4cef17ec32f6c8b8632af8
mgoeker (Markus Göker) <markus.goeker@dsmz.de>
2013-10-01 22:32:19 +0000

    minor additions to database support
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/opm@298 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- trunk/opmDB_in/inst/auxiliary/001_rdbms_drop.sql
+++ trunk/opmDB_in/inst/auxiliary/001_rdbms_drop.sql
@@ -2,8 +2,8 @@
 
 -- -----------------------------------------------------------------------------
 --
--- Code for removing the tables that hold PM data. Tested with PostgreSQL (9.1),
--- SQLite (3.7.9) and MySQL (5.5.32).
+-- Code for removing the tables that hold PM data in the correct order. Tested
+-- with PostgreSQL (9.1), SQLite (3.7.9) and MySQL (5.5.32).
 --
 -- -----------------------------------------------------------------------------
 
@@ -15,7 +15,6 @@ DROP TABLE IF EXISTS aggregated;
 DROP TABLE IF EXISTS aggr_settings;
 
 DROP TABLE IF EXISTS measurements;
+
 DROP TABLE IF EXISTS wells;
 DROP TABLE IF EXISTS plates;
-
-
