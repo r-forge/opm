9ee06ecc26743fd8134e8a48ea29099cb005e5a4
mgoeker (Markus Göker) <markus.goeker@dsmz.de>
2014-01-06 13:32:50 +0000

    additions to database I/O demos
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/opm@481 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- trunk/opmDB_in/demo/PostgreSQL_IO.R
+++ trunk/opmDB_in/demo/PostgreSQL_IO.R
@@ -10,6 +10,12 @@ conn <- dbConnect("PostgreSQL",
 
 result <- opm_dbcheck(conn)
 
+dbGetQuery(conn, c("ALTER TABLE plates ADD COLUMN strain text;",
+  "ALTER TABLE plates ADD COLUMN replicate integer;"))
+
+dbGetQuery(conn, c("ALTER TABLE plates DROP COLUMN strain;",
+  "ALTER TABLE plates DROP COLUMN replicate;"))
+
 dbDisconnect(conn)
 
 stopifnot(result == "ok")
--- trunk/opmDB_in/demo/SQLite_IO.R
+++ trunk/opmDB_in/demo/SQLite_IO.R
@@ -10,6 +10,13 @@ conn <- dbConnect("SQLite",
 
 result <- opm_dbcheck(conn)
 
+dbGetQuery(conn, c("ALTER TABLE plates ADD COLUMN strain text;",
+  "ALTER TABLE plates ADD COLUMN replicate integer;"))
+
+## one cannot drop columns from an SQLite table
+#dbGetQuery(conn, c("ALTER TABLE plates DROP COLUMN strain;",
+#  "ALTER TABLE plates DROP COLUMN replicate;"))
+
 dbDisconnect(conn)
 
 stopifnot(result == "ok")
