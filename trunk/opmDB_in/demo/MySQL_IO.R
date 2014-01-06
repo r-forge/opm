3685c8f4eca632642af7a0edb920192dce8d36ed
mgoeker <mgoeker@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2014-01-06 13:32:50 +0000

    additions to database I/O demos
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/opm@481 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- trunk/opmDB_in/demo/MySQL_IO.R
+++ trunk/opmDB_in/demo/MySQL_IO.R
@@ -9,6 +9,12 @@ conn <- dbConnect("MySQL", dbname = Sys.getenv("OPM_MYSQL_DB", "pmdata"))
 
 result <- opm_dbcheck(conn)
 
+dbGetQuery(conn, c("ALTER TABLE plates ADD COLUMN strain text;",
+  "ALTER TABLE plates ADD COLUMN replicate integer;"))
+
+dbGetQuery(conn, c("ALTER TABLE plates DROP COLUMN strain;",
+  "ALTER TABLE plates DROP COLUMN replicate;"))
+
 dbDisconnect(conn)
 
 stopifnot(result == "ok")
