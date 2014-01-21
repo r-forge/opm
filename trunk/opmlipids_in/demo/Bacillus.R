70c653b86246bd3cf88bf0942cd4cfb4cac28eb1
mgoeker <mgoeker@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2014-01-21 10:11:51 +0000

    * finished embedding CSS * documented it in vignette * full build of opm
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/opm@517 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- trunk/opmlipids_in/demo/Bacillus.R
+++ trunk/opmlipids_in/demo/Bacillus.R
@@ -1,6 +1,6 @@
 
 
-#' # Analyse a Bacillus fatty-acid data set provided together with the package
+#' # Analyse a Bacillus fatty-acid example data set
 #'
 #' Author: *Markus Goeker*
 
@@ -14,31 +14,49 @@ if (!length(files)) {
   stop("'opmlipids' example input files not found")
 }
 
+#' # Input and summarise data
+
 x <- read_rtf(files[grepl("Sikorski", basename(files))], include = NULL)
 print(summary(x))
 
 
-#' # Plot with transformation
+#' # Plot without transformation
 
 heat_map(x, "Sample ID", asqr = NA)
 
-#' # Remove the calibration plates, if any, and plot again
 
-if (any(is.calib <- x %q% c(Type = "Calib"))) {
-  x <- x[!is.calib]
-  heat_map(x, "Sample ID", asqr = NA)
-}
+#' # Plot with or without transformation
+
+heat_map(x, "Sample ID")
+
+#' The arcsine-square root transformation is recommended for proportion data
+#' under certain conditions. The code below takes care of the fact that here
+#' proportions are given as percent.
+
+heat_map(x, "Sample ID", asqr = NA, main = "With transformation")
 
 
+#' # Remove dubious measurements, if any, and plot again
+
+#' This code removes all calibration plates and all plates with less than 99%
+#' named peaks.
+#'
+x <- x[~ Type != "Calib" && as.numeric(`Percent Named`) >= 99]
+
+heat_map(x, "Sample ID", asqr = NA, main = "Cleaned data")
+
+
+
+#' # Manipulate metadata
+
+#' In many cases strain names are hidden in (= are part of) the sample ID.
+#'
 metadata(x) <- Strain ~ sub("^([^-]+-)+", "", `Sample ID`)
 
 
-#' # Create and plot principal coordinates
+#' # Create and plot principal components
 
 xpr <- prcomp(extract(x, "Strain"))
 biplot(xpr)
 
-#' Remove deviating measurement.
-xpr <- prcomp(extract(x[~ Strain != "SAGRA1"], "Strain"))
-biplot(xpr)
 
