d64e693fdddb668ca0254832257263c6b48c59c7
mgoeker <mgoeker@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2016-05-03 15:37:06 +0000

    [CHANGED] first adaptations to R 3.3.0
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/opm@784 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- trunk/pkgutils_in/R/imports.R
+++ trunk/pkgutils_in/R/imports.R
@@ -6,12 +6,22 @@
 # Dependencies
 #
 
+#' @importFrom grDevices col2rgb pdf
+#'
+NULL
+
+#' @import methods
+#'
+NULL
+
+#' @importFrom stats cmdscale dist na.fail
+#'
+NULL
 
 #' @importFrom tools toRd parse_Rd
 #'
 NULL
 
-
-#' @import methods
+#' @importFrom utils adist aspell file_test type.convert
 #'
 NULL
