bea6e0e165c45c59f78b5a82ccc957b533dfaa60
mgoeker <mgoeker@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2016-05-19 10:18:25 +0000

    [ADDED] method for cor.test() [ADDED] option to sort according to running time
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/opm@791 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- trunk/opm_in/R/plotting.R
+++ trunk/opm_in/R/plotting.R
@@ -1647,3 +1647,52 @@ setMethod("parallel_plot", c("OPMX", "ANY"), function(x, data, groups = 1L,
 
 
 ################################################################################
+
+
+#' Correlate wells
+#'
+#' This calculates all-against-all correlations between selected parameters
+#' inferred from the contained wells or correlations between all wells and
+#' selected wells of interest.
+#'
+#' @param x \code{\link{OPMS}} or \code{\link{MOPMX}} object. It is an error if
+#'   the \code{x} contains non-aggregated plates and if \code{\link{MOPMX}}
+#'   object contains \code{\link{OPM}} objects.
+#' @param y Optional vector with names or indexes of columns to correlate with
+#'   the full extracted matrix. Note that the names must refer to this generated
+#'   matrix, not to the well names in \code{x}.
+#' @param method Character scalar passed to \code{cor} from the \pkg{stats}
+#'   package.
+#' @param use Character scalar also passed to \code{cor}.
+#' @param subset Character scalar passed to \code{\link{extract}}.
+#' @param ... Optional arguments passed to \code{\link{extract}}.
+#' @export
+#' @return For the \code{\link{OPMS}} method, a squared matrix of correlations
+#'   if \code{y} is empty, another matrix with correlations otherwise. The
+#'   \code{\link{MOPMX}} method returns a list of such matrices.
+#' @details The resulting matrix can be plotted, e.g., using the \code{corrplot}
+#'   function from the \pkg{corrplot} package.
+#' @family plotting-functions
+#' @keywords regression
+#' @seealso stats::cor stats::cor.test corrplot::corrplot
+#' @examples
+#' (x <- cor.test(vaas_4[, , 1:10]))
+#' stopifnot(is.matrix(x), dim(x) == 10)
+#'
+setGeneric("cor.test")
+
+setMethod("cor.test", "OPMS", function(x, y = NULL, method = "pearson",
+    use = "everything", subset = opm_opt("curve.param"), ...) {
+  x <- extract(object = x, subset = subset, dataframe = FALSE, ci = FALSE,
+    as.groups = NULL, as.labels = NULL, ...)
+  if (length(y))
+    y <- x[, y, drop = FALSE]
+  cor(x, y, use, method)
+}, sealed = SEALED)
+
+setMethod("cor.test", "MOPMX", function(x, ...) {
+  oapply(object = x, fun = cor.test, simplify = FALSE, ...)
+}, sealed = SEALED)
+
+
+################################################################################
